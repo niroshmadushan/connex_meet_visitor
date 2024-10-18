f9010537cf6fbb96c2991dc0a41dfdcdba0d0cd9
Nirosh Madushan <127856472+niroshmadushan@users.noreply.github.com>
2024-10-18 14:58:28 +0530

    sedsfdsf

--- app/src/config.js
+++ app/src/config.js
@@ -1,19 +1,13 @@
-const BACKEND_URL = 'http://192.168.13.190:3001';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001'; // Fallback to localhost if not set in env
 
 const config = {
-    becomeapartnerapi:` ${BACKEND_URL}/becomePartner`,
-   
+    becomeapartnerapi: `${BACKEND_URL}/becomePartner`,
     loginapi: `${BACKEND_URL}/login`,
-  
     verifytoken: `${BACKEND_URL}/verifytoken`,
-
-    
     getallorgemails: `${BACKEND_URL}/getallorgemails`,
     getalllocationdata: `${BACKEND_URL}/getlocations`,
     getalluserdata: `${BACKEND_URL}/getProfileData`,
-
-    mainapi:`${BACKEND_URL}`
- 
+    mainapi: `${BACKEND_URL}`
 };
 
 export default config;
